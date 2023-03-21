600856368dbbe4700ecc1cfa2332726fc48f5ed5
Jorge Barros Medeiros <jorgebarrosextra@gmail.com>
2023-03-21 11:30:20 +0000

    intermediate files

---
+++ models/intermediate/cleansed_subscription.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+with cleansed_subscription as (
+    select * from {{ ref('subscriptions') }}
+)
+
+select 
+       sb.user_id,
+       sb.plan,
+       case 
+        when plan in ('Business','Diamond','Gold','Platinum','Premium') then 'High'
+        when plan in ('Bronze','Essential','Professional','Silver','Standard') then 'Normal'
+        else 'Low'
+       end as subscription_importance,
+       sb.status,
+       sb.payment_term,
+       sb.payment_method,
+       sb.subscription_term
+
+from cleansed_subscription as sb
